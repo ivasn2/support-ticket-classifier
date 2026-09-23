6c2463ce3986eb7979c379bdd9beb83277d310c8
ivasn2 <ivan.zharov0506@gmail.com>
2026-09-23 23:20:17 +0500

    feat(backend): init Spring Boot project with hello endpoint

---
+++ backend/src/test/java/com/example/ticket_classifier/TicketClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticket_classifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketClassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
